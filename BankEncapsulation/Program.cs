c9a86cd2ab9362209185d551e4be844a00a81bd4
Thai Vo <tahrbo@gmail.com>
2025-07-06 01:26:54 -0500

    Creating ATM menu

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            string choice = "1";
+
+            while (choice == "1") 
+            {
+                Console.WriteLine("Please enter your choice:");
+                Console.WriteLine("1: Check Balance");
+
+                Console.WriteLine("2: Add Money");
+
+                Console.WriteLine("3: Withdraw Funds");
+                Console.WriteLine("4: Exit");
+
+
+
+
+            }
+
         }
     }
 }
