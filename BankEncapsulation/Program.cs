db73aab57472093ae76d8f50c51c75acd315d4fe
Thai Vo <tahrbo@gmail.com>
2025-07-06 02:22:06 -0500

    Skeleton done

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -6,22 +6,64 @@
         {
             Console.WriteLine("Hello, World!");
 
-            string choice = "1";
+            string choice = "0";
 
-            while (choice == "1") 
+            while ((choice != "1") && (choice != "2") && (choice != "3") && (choice != "4")) 
             {
                 Console.WriteLine("Please enter your choice:");
                 Console.WriteLine("1: Check Balance");
-
                 Console.WriteLine("2: Add Money");
-
                 Console.WriteLine("3: Withdraw Funds");
                 Console.WriteLine("4: Exit");
 
+                choice = Console.ReadLine();
+                Console.WriteLine($"You chose {choice}");
 
 
 
             }
+
+            while (choice != "4")
+            {
+
+
+
+                switch (choice)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Balance");
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.WriteLine("Add");
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.WriteLine("Subtract");
+                            break;
+                        }
+                    case "4":
+                        {
+                            Console.WriteLine("Leave");
+                            break;
+                        }
+
+
+                }
+
+                Console.WriteLine("Please enter your choice:");
+                Console.WriteLine("1: Check Balance");
+                Console.WriteLine("2: Add Money");
+                Console.WriteLine("3: Withdraw Funds");
+                Console.WriteLine("4: Exit");
+
+                choice = Console.ReadLine();
+            }
+
+ 
 
         }
     }
--- BankEncapsulation/obj/Debug/net9.0/BankEncapsulation.AssemblyInfo.cs
+++ BankEncapsulation/obj/Debug/net9.0/BankEncapsulation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankEncapsulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40e52697594f761f6f6ee043959e503c3d9dc66e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9a86cd2ab9362209185d551e4be844a00a81bd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankEncapsulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankEncapsulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
