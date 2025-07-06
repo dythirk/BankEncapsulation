c0aab12a960123be082806b4e6a02d75914d956f
Thai Vo <tahrbo@gmail.com>
2025-07-06 02:54:18 -0500

    Ready

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount() { }
+
+        private double _balance = 0;
+
+        public void Deposit(double moneyToAdd)
+        {
+            _balance += moneyToAdd;
+        }
+
+        public void Withdraw(double moneyToTake)
+        {
+            _balance -= moneyToTake;
+        }
+
+        public double GetBalance() 
+        { 
+            return _balance; 
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,67 +4,73 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double transaction = 0;
+            BankAccount account = new BankAccount();
 
             string choice = "0";
 
-            while ((choice != "1") && (choice != "2") && (choice != "3") && (choice != "4")) 
+            while ((choice != "1") && (choice != "2") && (choice != "3") && (choice != "4"))    // Filtering responses for menu
             {
-                Console.WriteLine("Please enter your choice:");
+                Console.WriteLine("\nPlease enter your choice:");
                 Console.WriteLine("1: Check Balance");
-                Console.WriteLine("2: Add Money");
-                Console.WriteLine("3: Withdraw Funds");
-                Console.WriteLine("4: Exit");
+                Console.WriteLine("2: Deposit Money");
+                Console.WriteLine("3: Withdraw Money");
+                Console.WriteLine("4: Exit\n");
 
                 choice = Console.ReadLine();
-                Console.WriteLine($"You chose {choice}");
-
-
-
             }
 
-            while (choice != "4")
+            while (choice != "4")   // Executing repeating menu (if 4 was chosen, menu is bypassed and program terminates)
             {
-
-
-
                 switch (choice)
                 {
                     case "1":
                         {
                             Console.WriteLine("Balance");
+                            Console.WriteLine($"You have {account.GetBalance()} dollars.");
                             break;
                         }
                     case "2":
                         {
-                            Console.WriteLine("Add");
+                            Console.WriteLine("Deposit");
+                            Console.WriteLine("Please enter amount to deposit:");
+                            var canParse = double.TryParse(Console.ReadLine(), out transaction);
+                            while (canParse == false)
+                            {
+                                Console.WriteLine("Please enter a valid amount:");
+                                canParse = double.TryParse(Console.ReadLine(), out transaction);
+                            }
+                            account.Deposit(transaction);
                             break;
                         }
                     case "3":
                         {
-                            Console.WriteLine("Subtract");
+                            Console.WriteLine("Withdrawl");
+                            Console.WriteLine("Please enter amount to withdraw:");
+                            var canParse = double.TryParse(Console.ReadLine(), out transaction);
+                            while (canParse == false)
+                            {
+                                Console.WriteLine("Please enter a valid amount:");
+                                canParse = double.TryParse(Console.ReadLine(), out transaction);
+                            }
+                            account.Withdraw(transaction);
                             break;
                         }
                     case "4":
                         {
-                            Console.WriteLine("Leave");
+                            Console.WriteLine("Exiting");
                             break;
                         }
-
-
                 }
 
-                Console.WriteLine("Please enter your choice:");
+                Console.WriteLine("\nPlease enter your choice:");
                 Console.WriteLine("1: Check Balance");
                 Console.WriteLine("2: Add Money");
                 Console.WriteLine("3: Withdraw Funds");
-                Console.WriteLine("4: Exit");
+                Console.WriteLine("4: Exit\n");
 
                 choice = Console.ReadLine();
             }
-
- 
-
         }
     }
 }
